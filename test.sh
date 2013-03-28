4f15857fe70786726725ab6d7e8f95a21c3323f3
Solomatov A.A <aso@tlt.ru>
2013-03-28 11:35:40 +0400

    => folding Insert Folding section in GRUB configuration file (c) aso, v.1.3.9 by 23.01.2013. Little cleaning code.
    === Development service utility (temporary) ===
    
    => foldbg
    launch the Sectioneer for debug
    v.0.3.0 from 23.01.2013. by aso
    
    => test.sh
    The test of shell programming
    (c) aso, v.0.5.0 by 23.01.2013
    
    => tst2.sh
    The test of shell programming
    from 23.01.2013.

---
+++ test.sh
@@ -0,0 +1,111 @@
+#! /bin/sh
+set -e
+
+# The test of shell programming
+
+
+. "./foldlib"
+
+
+#=[ const part ]===============================================================
+
+#sysconfdir="/etc"
+#grub_mkcfg_dir="${sysconfdir}/grub.d"
+grub_mkcfg_dir="cfg-test"
+
+
+#=[ end of const part ]========================================================
+
+
+
+
+# Calling from folding marker 
+_infolding='Yes'
+export _infolding
+
+# Insert folding section in config file function
+# Parameters:
+#   $1 - OS class
+remark_insert()
+{
+## control string for sed for search control comment: '### Begin...' & insert file name
+#echo "
+#/$(mark "BEGIN\ $(echo $grub_mkcfg_dir/$(sect_fn $1 $p)| sed 's/\//\\&/'g)")/ r $(sect_fn $1 $p)
+#/$(mark "BEGIN\ $(echo $grub_mkcfg_dir/$(sect_fn $1 $e)| sed 's/\//\\&/'g)")/ r $(sect_fn $1 $e)
+#"
+echo "
+/$(fullmark $BEG $(sect_fn $1 $p))/ r $(sect_fn $1 $p)
+/$(fullmark $BEG $(sect_fn $1 $e))/ r $(sect_fn $1 $e)
+"
+} # remark_insert()------------------------------------------------------------
+
+# Insert folding section in config file function
+# Parameters:
+#   $1 - OS class (win/gentoo)
+#   $2 - sect class (prolog/epilog)
+remark_insert2()
+{
+## control string for sed for search control comment: '### Begin...' & insert file name
+#echo "
+#/$(mark "BEGIN\ $(echo $(sect_fn $1 $p)| sed 's/\//\\&/'g)")/ r $(sect_fn $1 $p)
+#/$(mark "BEGIN\ $(echo $(sect_fn $1 $e)| sed 's/\//\\&/'g)")/ r $(sect_fn $1 $e)
+#"
+echo "
+/$(fullmark $BEG\ $(sect_fn $1 $p))/! b inner
+
+#p
+n
+
+/$(fullmark $EN $(sect_fn $1 $p))/ {
+i \\
+$($grub_mkcfg_dir/$(sect_fn $1 $p))
+}
+
+:inner
+
+#p
+
+:end
+"
+} # remark_insert2()-----------------------------------------------------------
+
+
+# Safe string
+safe="!!!Reserved!!!"
+
+# Insert folding section in config file function
+# on file insertion & full control of output (-n option)
+# Parameters:
+#   $1 - OS class (win/gentoo)
+#   $2 - sect class (prolog/epilog)
+final_mark()
+{
+${grub_mkcfg_dir}/$(sect_fn $1 $2) > "$tmprefix$(sect_fn $1 $2)"
+sed -e "/$(fullmark $BEG $(sect_fn $1 $2))${safe}/ {
+r $tmprefix$(sect_fn $1 $2)
+s/${safe}//
+}"
+rm -f "tmprefix$(sect_fn $1 $2)"
+} # final_mark()---------------------------------------------------------------
+
+
+
+echo '==========================================================================================\n'
+#echo '$win insertion'
+#remark_insert $win
+
+#echo '---------------------------------------------------\n'
+#echo '$gen insertion'
+##remark_insert $gen
+#remark_insert2 $gen
+
+#sed  -ne "$(remark_insert2 $win $p)" | sed -ne "$(remark_insert2 $win $e)"
+final_mark $win $p | final_mark $win $e |
+final_mark $gen $p | final_mark $gen $e
+
+
+#remark_insert $win
+#remark_insert2 $win
+#final_mark $win $p |
+#final_mark $win $e
+#sed -e "$(remark_insert3 $win $e)" |...
