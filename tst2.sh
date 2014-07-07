59c8824582b95bf60b0a1f03f04b8fa9abbad8a5
Solomatov <aso@tlt.ru>
2014-07-07 11:11:41 +0400

    Corect merging from branch 'feature-shield-w-param-v.1'.

--- tst2.sh
+++ tst2.sh
@@ -15,11 +15,11 @@ export _dev
 
 # The test of shell programming
 
-out_cfg='babc ddu -rrt'
+#out_cfg='babc ddu -rrt'
 #
-echo "out_cfg is '$out_cfg', len ${#out_cfg}"
-#echo "out_cfg is '${out_cfg:0}'"
-echo `expr "$out_cfg" : '-.'`       # 8
+#echo "out_cfg is '$out_cfg', len ${#out_cfg}"
+##echo "out_cfg is '${out_cfg:0}'"
+#echo `expr "$out_cfg" : '-.'`       # 8
 #if ! expr "$out_cfg" : "-."> /dev/null; then
 ##if [ "za" = "z." ]; then
 ##if test "x${out_cfg}" = "x"; then
@@ -33,22 +33,66 @@ echo `expr "$out_cfg" : '-.'`       # 8
 # ------------------------------------------------------------------------------
 
 
+#echo_final()
+#{
+#echo "/$(fullmark $BEG $(sect_fn $1 $2))$safe/ {
+#r $tmprefix$(sect_fn $1 $2)
+#s/${safe}//
+#}"
+##r $tmprefix$(basename $(sect_fn $1 $2))
+#
+#} # echo_final()---------------------------------------------------------------
+
+
+## Echoing string for markup section in config file function
+## Parameters:
+##   $1 - OS class (gen, win...)
+#echo_remark()
+#{
+#
+#echo "
+#/\([^#]*.*menuentry\)\([^#].*$(o_name $1)\)/! b end  # detect start of section /menuentry <OS_Name>/
+#
+#i\\
+#\\\n$(fullmark $BEG $(sect_fn $1 $p))$safe
+#i\\$(fullmark $EN $(sect_fn $1 $p))\\\n
+#
+#:consect		# continue sampling section
+#n
+#/^}/! b consect
+#
+#:interch		# final of section. Sampling iterval between sections
+#n
+#/[^#]*.*menuentry/ {	# detect new section
+#/[^#]*.*$(o_name $1)/ b consect	# new section is desired
+#b close			# new section is not desired
+#}
+#/### \($BEG\)\|\($EN\)/ b close	# detect new section, marked by control comments
+#b interch
+#
+#:close
+#
+#i\\
+#\\\n$(fullmark $BEG $(sect_fn $1 $e))$safe
+#i\\$(fullmark $EN $(sect_fn $1 $e))\\\n
+#
+#:end
+#"
+#
+#} # echo_remark() -------------------------------------------------------------------------
 
 ## Echoing string for test regexp & all for section markup
 ## Parameters: None
 ##   $1 - ...
 echo_test()
 {
-	fullavoidcmt='/\(\([^{]*\\n\)\?[^#{\\n]\+\)\?/'
-#	blkcmt='([^#{\\n]*)'
-#	blkcmt='((.*\\n)?[^#\\n]+)?'
 #   первый рабочий вариант выражения, отбрасывающий комментарии
 #	blkcmt='(.*\\n[^#\\n]*)?'
 #   второй работающий вариант регекспа, исключающий комментарии перед обънктом
 #   но допускающий множество произвольных строк до того.
 #	blkcmt='([^#\\n]*(#[^\\n]*)?\\n)*[^#\\n]*'
 #   третий вариант - для вложенных скобок,
-#   предовращает появление '{' и '}' на защищаемых интервалах.
+#   не допускает появления '{' и '}' на защищаемых интервалах.
 	blkcmt='([^{#}\\n]*(#[^\\n]*)?\\n)*[^{#}\\n]*'
 #	blkcmt='([^#\\n]*)|(.*\\n[^#\\n]*)'
 	echo "$(shield1 $blkcmt)" >&2
@@ -65,11 +109,9 @@ cat << EOF
 :strtsmpl
 #  /\n}/! b presample
 #  /{.*}/! b presample
-#  /^[^#{\n]*{[^#{\n]*}/! b presample
 #  /^$(shield1 $blkcmt{$blkcmt})/! b presample
   /^$(shield1 "$blkcmt{($blkcmt{($blkcmt{$blkcmt})*$blkcmt})*$blkcmt}")/! b presample
 #  /^$(shield1 $blkcmt})/! b
-#  /\(^\|\n\)[^#]*{\(.*\n\)\\?[^#]*}/! b presample
   $ b
 #-------------------------------------
 	#  /\n}/! b presample
@@ -129,7 +171,7 @@ shield()
 
 
 #
-# Shielding
+# Shielding with parameter analyses
 # for using in sed-scripts
 # Simplified sintaxis:
 #	any symbols from set: {| (+ )?} -> \{original_symbol}
@@ -246,8 +288,45 @@ shield3()
 } # shld3
 
 
+
+echo 'Mark'
+
+#echo "$(fullmark $BEG $(sect_fn $gen $p))"
+#echo "$(fullmark $BEG $(sect_fn $win $e))"
+#echo "$(shield '### /abc/def/(ghi|klmn) ###')"
+echo "gen: $gen"
+echo "p: $p"
+echo "BEG: $BEG"
+#echo "$(fullmark $BEG _$gen-$p)"
+echo "$(shield "$(fullmark $BEG _$gen-$p)")"
+#echo "$(shield '### /abc/def/(ghi|klmn) ###')"
+echo '==[ Remark ]=============================================================\n'
+
 #echo_remark $win
 #sed -e "/aaa/!b;n;s/\(# exec!\)\($(shldslash ${grub_mkcfg_dir}/$(sect_fn $1 $2))\)#.*/\2 -i/e"
+
+# Реализация замены имени файла через exec-комментарий (командный комментарий),
+# опция исполнения файла задаётся в сценарии
+#sed -e "/aaa/!b;n;s/\(# exec\!\)\($(shldslash '${grub_mkcfg_dir}/$(sect_fn $gen $p)')\)#.*/\2 -i/e"
+#sed -e "s/\(# exec\!\)\($(shldslash ${grub_mkcfg_dir}/$(sect_fn $gen $p))\)#.*/\2 -i/e"
+#sed "s/\(# exec\!\)\($(shldslash ${grub_mkcfg_dir}/$(sect_fn $gen $p))\)#.*/\2 -i/e"
+#echo "s/\(# exec\!\)\($(shldslash ${grub_mkcfg_dir}/$(sect_fn $gen $p))\)#.*/\2 -i/e"
+
+# Замена имени исполняемого файла на вывод его исполнения,
+# вариант с переменной сценария, и одновременным выводом через команду echo
+# В заменяющей части команды s/// - \2 - добавлен лишний бэкслэш: \\2
+#teta="s/\(# exec\!\)\($(shldslash ${grub_mkcfg_dir}/$(sect_fn $gen $p))\)#.*/\\2 -i/e"
+#sed -e "${teta}"
+#echo "$teta"
+
+
+# Вариант со считыванием опции исполнения сценария из командного комментария.
+# Сценарий в переменной shell.
+##teta="s/\(# exec!\)\($(shldslash ${grub_mkcfg_dir}/$(sect_fn $gen $p))\)\([^#]*\)/\\2 -i /e"
+#teta="s/\(# exec!\)\($(shldslash ${grub_mkcfg_dir}/$(sect_fn $win $p))\)\([^#]*\)/\\2\\3 /e"
+#sed -e "${teta}"
+##sed -e "/aaa/!b;n;s/\(# exec!\)\($(shldslash ${grub_mkcfg_dir}/$(sect_fn $gen $p))\)\([^#]*\)/\2 -i/e"
+#echo "${teta}"
 
 
 # Вариант: замена сразу обоих типов файлов секций - _[gen,win]-prolog за один проход sed
@@ -255,7 +334,7 @@ shield3()
 #sed -e "s/\(# exec!\)\($(shldslash ${grub_mkcfg_dir}/)\)_\($gen\|$win\)-\($p\)#.*/\2_\3-\4 -i/e"
 #echo "/aaa/!b;n;s/\(# exec!\)\($(shldslash ${grub_mkcfg_dir}/)\)_\($gen\|$win\)-\($p\)#.*/\2_\3-\4 -i/e"
 
-#sed $allopts -e "$(echo_test)"
+sed $allopts -e "$(echo_test)"
 
 #echo '==[ echo_test ]==========================================================\n'
 #echo "$(echo_test)"
@@ -271,9 +350,8 @@ shield3()
 # cat sect_extracted
 # echo ''
 
-echo '==[ Shield ]============================================================\n'
-#shield '(+abc+cde)?rlq+(dfg)(gge|uud)?\n abc&+cde&?&+&(gfk&|dfe&)\n&&qqq&&+&&(ppp&&|mrm?&)'
-shield2 $* '(+abc+cde)?rlq+(dfg)(gge|uud)?\n abc\+cde\?\+\(gfk\|dfe\)\n\\qqq\\+\\(ppp\\|mrm?\)\nabba\babba\\bambra\carramba'
+echo '==[ Shield1 ]============================================================\n'
+shield1 '(+abc+cde)?rlq+(dfg)(gge|uud)?\n abc&+cde&?&+&(gfk&|dfe&)\n&&qqq&&+&&(ppp&&|mrm?&)'
 #aaa=$(shield 'abba\\nbabba')
 # aaa=$(shield2 $* 'abba\\nbabba')
 # echo "$aaa" >&2
